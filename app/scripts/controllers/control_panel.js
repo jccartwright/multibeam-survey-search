4ceae7a32ee9a18ebecd2e6d18a7cdd1e827bcbd
John Cartwright <john.c.cartwright@noaa.gov>
2017-04-11 14:11:46 -0600

    remove debugging statements and dead code

--- app/scripts/controllers/control_panel.js
+++ app/scripts/controllers/control_panel.js
@@ -33,52 +33,6 @@ angular.module('multibeamApp').controller('ControlPanelCtrl', ['$scope', '$http'
       console.error('unexpected viewMode');
     }
   };
-
-/*
-    //syntax required to get async results from service.  in practice, by the 
-    //time the UI is accessed, the async functions are complete and the arrays
-    //can be accessed directly from the service
-    $scope.$watch(function() {
-      return repository.instruments;
-    }, function(newValue) {
-        $scope.instruments = newValue;
-    }, true);
-*/
-/*
-    $http.get('https://maps.ngdc.noaa.gov/mapviewer-support/multibeam/instruments.groovy').then(function(response){
-      $scope.instruments = response.data;
-    });
-
-    $http.get('https://maps.ngdc.noaa.gov/mapviewer-support/multibeam/platforms.groovy').then(function(response){
-      $scope.platforms = response.data;
-    });
-
-    $http.get('https://maps.ngdc.noaa.gov/mapviewer-support/multibeam/sources.groovy').then(function(response){
-      $scope.sources = response.data;
-    });
-
-    $http.get('https://maps.ngdc.noaa.gov/mapviewer-support/multibeam/departureports.groovy').then(function(response){
-      $scope.departurePorts = response.data;
-    });
-
-    $http.get('https://maps.ngdc.noaa.gov/mapviewer-support/multibeam/arrivalports.groovy').then(function(response){
-      $scope.arrivalPorts = response.data;
-    });
-
-    $http.get('https://maps.ngdc.noaa.gov/mapviewer-support/multibeam/chiefscientists.groovy').then(function(response){
-      $scope.chiefScientists = response.data;
-    });
-
-    $scope.getSurveyNames = function(val) {
-      return $http.get('https://maps.ngdc.noaa.gov/mapviewer-support/multibeam/surveynames.groovy', {
-        params: {
-          name: val.toUpperCase()
-        }
-      }).then(function(response){
-        return response.data;
-      });
-    };
-*/
     
   $http.get('https://maps.ngdc.noaa.gov/mapviewer-support/multibeam/startdates.groovy').then(function(response){
     //expects string in format of YYYY-MM-DDTHH:MM:SSZ
@@ -114,43 +68,13 @@ angular.module('multibeamApp').controller('ControlPanelCtrl', ['$scope', '$http'
   $scope.popup2 = {
     opened: false
   };
-  
-  // $scope.selectedValues = {
-  //   surveyName: null,
-  //   shipName: null,
-  //   shipSource: null,
-  //   instrument: null,
-  //   chiefScientist: null,
-  //   departurePort: null,
-  //   arrivalPort: null,
-  //   startDate: null,
-  //   endDate: null
-  // };
-
-  // $scope.$watchCollection('selectedValues', function() {
-  //   //console.log($httpParamSerializer($scope.selectedValues));
-  //   //TODO change style of "Get Data" button?
-  // });
-  /*
-  $scope.$watchGroup(["selectedSurvey", "selectedShip", "selectedSource", 
-    "selectedInstrument", "selectedChiefScientist", "selectedDeparturePort",
-    "selectedArrivalPort", "selectedStartDate", "selectedEndDate"], function(newValue, oldValue) {
-      console.log(newValue);
-  });
-*/
-  
+    
   $scope.surveySelectHandler = function() {
     console.log('inside surveySelectHandler');
-    //$scope.selectedValues.survey = item;
-    //console.log(selectedValues);
-    //console.log(item);
-    //$scope.getDataBtnHandler();
     $scope.repository.getData();
-    console.log('selectedValues: ',$scope.repository.selectedValues);
   };
 
   $scope.getDataBtnHandler = function() {
-    console.log('inside getDataBtnHandler...');
     repository.getData();
     // console.log($scope.selectedValues);
     // $http.get('https://maps.ngdc.noaa.gov/mapviewer-support/multibeam/surveyattributes.groovy', {
@@ -166,11 +90,4 @@ angular.module('multibeamApp').controller('ControlPanelCtrl', ['$scope', '$http'
     $scope.repository.data = [];
   };
 
-  
-
-/*
-  $scope.gridOptions.data = [
-    {"NGDC_ID":"NEW2528","CHIEF_SCIENTIST":"Kennedy, Brian","DEPARTURE_PORT":"Honolulu, HI","ARRIVAL_PORT":"Honolulu, HI","START_DATE":"2016-12-01","END_DATE":"2016-12-08","SURVEY_NAME":"EX1608","SHIP_NAME":"Okeanos Explorer","SOURCE":"NOAA Office of Ocean Exploration and Research (OER)","INSTRUMENT":"Kongsberg EM302","DOWNLOAD_URL":"https://www.ngdc.noaa.gov/ships/okeanos_explorer/EX1608_mb.html"},
-    {"NGDC_ID":"NEW2529","CHIEF_SCIENTIST":"Sowers, Derek","DEPARTURE_PORT":"Honolulu, HI","ARRIVAL_PORT":"Honolulu, HI","START_DATE":"2016-12-10","END_DATE":"2016-12-16","SURVEY_NAME":"EX1609","SHIP_NAME":"Okeanos Explorer","SOURCE":"NOAA Office of Ocean Exploration and Research (OER)","INSTRUMENT":"Kongsberg EM302","DOWNLOAD_URL":"https://www.ngdc.noaa.gov/ships/okeanos_explorer/EX1609_mb.html"}]
-*/   
   }]);
